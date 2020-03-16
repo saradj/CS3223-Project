8433702ce55accbcac19040cc0852b66a8f3e563
saradj <43466740+saradj@users.noreply.github.com>
2020-03-16 16:57:07 +0800

    initial

---
+++ src/qp/operators/BlockNestedJoin.java
@@ -0,0 +1,8 @@
+package qp.operators;
+
+public class BlockNestedJoin extends Join {
+
+    public BlockNestedJoin(Operator left, Operator right, int type) {
+        super(left, right, type);
+    }
+}
